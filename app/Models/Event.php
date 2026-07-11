266382a9ea43d64c35d236e61251dbab64a11727
Pasyah Vegananda <32693282+PVegananda@users.noreply.github.com>
2026-07-11 09:21:40 +0700

    Initial commit

---
+++ app/Models/Event.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'deskripsi',
+        'tanggal',
+        'lokasi',
+        'gambar',
+    ];
+
+    protected $casts = [
+        'tanggal' => 'datetime',
+    ];
+
+    public function tikets()
+    {
+        return $this->hasMany(Tiket::class);
+    }
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function order()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
+
